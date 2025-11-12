02cd635a12adffb51c09fe87e65801db44d74b9b
Crhistiko12 <almanzacoorp@gmail.com>
2025-11-12 16:10:46 -0400

    Se hizo cambio a español

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
 
   # ENUMS CORREGIDOS - nueva sintaxis
-  enum :status, { pending: 0, in_progress: 1, completed: 2 }
-  enum :priority, { low: 0, medium: 1, high: 2 }
+  enum :status, { pendiente: 0, en_progreso: 1, completado: 2 }
+  enum :priority, { baja: 0, media: 1, alta: 2 }
 
   validates :title, presence: true
   validates :due_date, presence: true
@@ -15,6 +15,6 @@ class Task < ApplicationRecord
 
   # Método de instancia
   def overdue?
-    due_date < Time.current && status != 'completed'
+    due_date < Time.current && status != 'completado'
   end
 end
